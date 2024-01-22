439a7b88a1a18abcd8a5b10c6d01d40505e8c9bf
Arturo Zepeda <aaze92@gmail.com>
2024-01-21 21:15:00 -0600

    aññado correccion de siempre

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ app.use(cors())
 app.use(express.json());
 app.use('/api', routes);
 app.listen(3000, () => {
-    console.log(`Server Started at ${3000}`)
+    console.log(`Server Started at ${3001}`)
 })
