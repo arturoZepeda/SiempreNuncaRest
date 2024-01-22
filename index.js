2b9dd3800193347ef09c6527997dccd1618a9944
Arturo Zepeda(server) <aaze92@gmail.com>
2024-01-22 03:16:37 +0000

    cambio depuerto

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use('/api', routes);
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log(`Server Started at ${3001}`)
 })
